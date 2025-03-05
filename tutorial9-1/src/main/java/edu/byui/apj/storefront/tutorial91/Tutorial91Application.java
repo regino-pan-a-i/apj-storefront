0eeb99caf68f1a129f048f26af460a990559bd15
Andre Regino <reginopanai@gmail.com>
2025-03-04 21:08:40 -0700

    In-class activity finished week 9

---
+++ tutorial9-1/src/main/java/edu/byui/apj/storefront/tutorial91/Tutorial91Application.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial91;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial91Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial91Application.class, args);
+	}
+
+}
---
+++ tutorial9-1/src/test/java/edu/byui/apj/storefront/tutorial91/Tutorial91ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial91;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial91ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
