b3081c26d849658e7e0e58e854bcd574c0f27d07
Andre Regino <reginopanai@gmail.com>
2025-03-06 21:47:45 -0700

    In-class activity finished week 9-2

---
+++ tutorial9-2/src/main/java/edu/byui/apj/storefront/tutorial92/model/Animal.java
@@ -0,0 +1,11 @@
+package edu.byui.apj.storefront.tutorial92.model;
+import lombok.Data;
+
+@Data
+public class Animal {
+    private int id;
+    private String name;
+    private int age;
+    private String diet;
+    private String species;
+}
