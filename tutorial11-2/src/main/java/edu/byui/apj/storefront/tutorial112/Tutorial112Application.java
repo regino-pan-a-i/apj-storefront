ec7979011245994dcad1e9daebf7af87d5ff42f3
Andre Regino <reginopanai@gmail.com>
2025-03-25 01:09:23 -0600

    Updated db module

---
+++ tutorial11-2/src/main/java/edu/byui/apj/storefront/tutorial112/Tutorial112Application.java
@@ -0,0 +1,15 @@
+package edu.byui.apj.storefront.tutorial112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Tutorial112Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tutorial112Application.class, args);
+    }
+
+}
---
+++ tutorial11-2/src/test/java/edu/byui/apj/storefront/tutorial112/Tutorial112ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial112;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial112ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
